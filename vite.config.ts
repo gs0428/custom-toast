d7f39ff6b842a59a0149f90949ff9029988f1535
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-10-28 19:07:23 +0900

    chore: init vite-plugin-css-injected-by-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
