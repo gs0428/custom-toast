cf8f4ceb3968ff90c29ce6e5b3b75b73239ace88
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-10-28 18:40:56 +0900

    chore: 번들로 vite로 변경

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "Toast",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+    minify: true,
+  },
+});
